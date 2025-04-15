b7819280fdb508dd67dcdb7f23abc8642c12af41
Pancow4 <pancowteam4@gmail.com>
2025-04-15 13:38:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,76 @@
-document.addEventListener("keydown", start);
-
-var walkImage = 1;
-var walkWorker = 0;
-var time = 50;
-var timeWorker = 0;
-var runImage = 1;
-var runWorker = 0;
-var knightMarginLeft = 80;
-var isRun = false;
-var attackImage = 1;
-var attackWorker = 0;
-var attackCount = 0;
-
-var runSound = new Audio("run.mp3");
-runSound.loop = true;
-
-var attackSound = new Audio("attack.mp3");
-
-function start(event) {
-  if (event.key == "Enter" && walkWorker == 0) {
-    walk();
-    timeRemain();
-    run();
-    runSound.play();
+function start(event){
+if(event.key=="Enter"  && walkWorker==0){
+   walk();
+   timeRemain();
+   run();
+   runSound.play();
+}
+  if (event.key==" "){
+   if (attackWorker == 0 && runWorker ==0 && isRun == true)  {
+   attack();
+   attackSound.play();
   }
 
-  if (event.key == " ") {
-    if (attackWorker == 0 && runWorker == 0 && isRun == true) {
-      attack();
-      attackSound.play();
-    }
+}
+}
+var walkImage=1;
+var walkWorker=0;
+function walk(){
+   walkWorker=setInterval(()=> {
+   walkImage++
+if (walkImage==11) {walkImage=1;}
+   document.getElementById("1").src="Walk"+walkImage+".png";
+   },150);
+}
+ var time=50;
+ var timeWorker= 0;
+  function timeRemain() {
+    timeWorker = setInterval(()=>{      
+      time--
+      if(time==0) {
+         alert("Game Over");
+         window.location.reload();
+      }
+      document.getElementById("time").innerHTML = "Your Remaining Time:"+time;
+    },500);
+
+
+
+
   }
-}
-
-function walk() {
-  walkWorker = setInterval(() => {
-    walkImage++;
-    if (walkImage == 11) walkImage = 1;
-    document.getElementById("1").src = "Walk" + walkImage + ".png";
-  }, 150);
-}
-
-function timeRemain() {
-  timeWorker = setInterval(() => {
-    time--;
-    if (time == 0) {
-      alert("Game Over");
-      window.location.reload();
-    }
-    document.getElementById("time").innerHTML = "Your Remaining Time: " + time;
-  }, 500);
-}
-
-function run() {
-  isRun = true;
-  runWorker = setInterval(() => {
-    runImage++;
-    if (runImage == 11) runImage = 1;
-
-    if (knightMarginLeft < 1000) {
-      knightMarginLeft += 10;
-      document.getElementById("12").style.marginLeft = knightMarginLeft + "px";
-    }
-
-    if (knightMarginLeft >= 1000) {
-      clearInterval(runWorker);
-      runWorker = 0;
-      runSound.pause();
-      attack();
-      attackSound.play();
-    }
-
-    document.getElementById("12").src = "Run" + runImage + ".png";
-  }, 100);
-}
-
-function attack() {
+  var runImage=1;
+  var runWorker= 0;
+  var knightMarginLeft=80;
+  var isRun = false;
+  var runSound = new Audio("run.mp3");
+  runSound.loop =true;
+   function run() {
+      isRun = true ;
+     runWorker = setInterval(()=>{      
+       runImage++
+       if (knightMarginLeft < 1000 ){
+         knightMarginLeft = knightMarginLeft+ 10;
+         document.getElementById("12").style.marginLeft = knightMarginLeft+ "px";
+       }
+        if (knightMarginLeft== 1000) {
+         clearInterval(runWorker);
+         runWorker=0;
+         runSound.pause();
+         attack();
+         attackSound.play();
+        }
+       if(runImage==11) {
+         runImage=1;
+       }
+       document.getElementById("12").src ="Run"+runImage+".png";
+     },100);
+   }
+    var attackImage =1 ;
+    var attackWorker =0;
+    var attackCount =0 ;
+    var attackSound =new Audio ("attack.mp3");
+      
+  function attack() {
   attackWorker = setInterval(() => {
     attackImage++;
     if (attackImage == 11) {
@@ -86,14 +81,12 @@ function attack() {
     document.getElementById("12").src = "Attack" + attackImage + ".png";
   }, 150);
 
+  // 🔥 Add this part:
   attackCount++;
-  const shots = document.getElementById("shots");
-  if (shots) {
-    shots.innerHTML = "Shots Fired: " + attackCount;
-  }
+  document.getElementById("shots").innerHTML = "Shots Fired: " + attackCount;
 
   if (attackCount == 5) {
-    alert("You won!");
+    alert("You won fucker");
     window.location.reload();
   }
 }
